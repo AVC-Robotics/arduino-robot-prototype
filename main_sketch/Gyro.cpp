916e0985fd440d8a1bbb2b0f3be33340227a640b
clausworks <clausworks@zoho.com>
2019-11-07 22:59:12 -0800

    Creatd Gyro class
    Interfaces with MPU6050

---
+++ main_sketch/Gyro.cpp
@@ -0,0 +1,72 @@
+// Gyro
+// This class interfaces with an MPU6050 3-axis gyroscope to get the angle
+// displacement.
+
+// Code largely taken from bit.ly/2PXP7Aw
+
+#include "Gyro.h"
+
+// const int MPU_ADDR = 0x68;
+// int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+// int minVal=265;
+// int maxVal=402;
+
+// double yaw;
+// double pitch;
+// double roll;
+
+
+
+// constructor
+Gyro::Gyro() {}
+
+
+void Gyro::initialize(int pinSDA, int pinSCL) {
+    Wire.begin();
+    Wire.beginTransmission(MPU_ADDR);
+    Wire.write(0x6B);
+    Wire.write(0);
+    Wire.endTransmission(true);
+}
+
+
+// update x,y,z, with latest values
+void Gyro::update() {
+    Wire.beginTransmission(MPU_ADDR);
+    Wire.write(0x3B);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_ADDR,14,true);
+    AcX=Wire.read()<<8|Wire.read();
+    AcY=Wire.read()<<8|Wire.read();
+    AcZ=Wire.read()<<8|Wire.read();
+    int xAng = map(AcX,minVal,maxVal,-90,90);
+    int yAng = map(AcY,minVal,maxVal,-90,90);
+    int zAng = map(AcZ,minVal,maxVal,-90,90);
+
+    // update member variables with values
+    x = RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
+    y = RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
+    z = RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
+}
+
+
+
+
+////////////////////////////////
+// ANGLE DISPLACEMENT GETTERS //
+////////////////////////////////
+
+
+double Gyro::getX() {
+    return x;
+}
+
+
+double Gyro::getY() {
+    return y;
+}
+
+
+double Gyro::getZ() {
+    return z;
+}
---
+++ main_sketch/Gyro.h
@@ -0,0 +1,31 @@
+#ifndef GYRO_H
+#define GYRO_H
+
+#include <Arduino.h>
+#include <Wire.h>
+
+class Gyro {
+private:
+    const int MPU_ADDR = 0x68;
+    int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+    int minVal=265;
+    int maxVal=402;
+
+    double x;
+    double y;
+    double z;
+//
+public:
+    Gyro();
+
+    void initialize(int pinSDA, int pinSCL);
+
+    void update();
+
+    // position getters
+    double getX();
+    double getY();
+    double getZ();
+};
+
+#endif
